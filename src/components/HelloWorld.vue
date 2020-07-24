9086845815d76659a0cb3a2e7999cb83209025a4
YeFan <919024797@qq.com>
2020-07-24 17:02:32 +0800

    feat(考场安排): 增加了考场序号范围功能

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,6 +20,7 @@
     <p></p>
     <el-input v-model="testNumber" placeholder="请输入考生起始准考证号"></el-input>
     <p></p>
+
     <!-- 按钮 -->
     <el-upload
             class="upload"
@@ -30,10 +31,12 @@
             :http-request="httpRequest">
       <el-button size="small" type="primary">上传考场名单表</el-button>
     </el-upload>
-    <br />
-    <el-button style="margin-bottom: 10px" type="primary" @click="assignedExamination">分配考场</el-button>
-    <br />
-    <el-button style="margin-bottom: 10px" type="primary" @click="exportExcel">导出excel</el-button>
+    <div class="button-group">
+      <el-button style="margin-bottom: 10px" type="primary" @click="assignedExamination">分配考场</el-button>
+      <el-button style="margin-bottom: 10px" type="primary" @click="exportExcel">导出准考证excel</el-button>
+      <el-button style="margin-bottom: 10px" type="primary" @click="exportRoomExcel">导出考场安排excel</el-button>
+      <el-button style="margin-bottom: 10px" type="primary" @click="resetData">重置所有数据</el-button>
+    </div>
     <!-- 按钮 end -->
 
     <div class="header">
@@ -79,12 +82,14 @@
         title: '2020年度襄阳市市直学校公开招聘笔试座次表', // 标题
         testSubjectA: '职业能力倾向测验', // 考试科目
         testSubjectB: '综合应用能力', // 考试科目
-        testTimeA: '8:00 - 9:30',
-        testTimeB: '9:30 - 11:00',
+        testTimeA: '8:30 - 10:00',
+        testTimeB: '10:00 - 12:00',
         picUrl: 'logo.png',
         roomNo: null, // 考场起始序号， 用来处理几个分类考试在同一时间同一个考点进行考试的情况
         testNumber: null, // 考生准考证起始序号
-        dataToExcel: [], // 待导出为excel的数据
+        dataToExcel: [], // 待导出为准考证excel的数据
+        dataToRoomSetExcel: [], // 待导出为考场安排excel，为重置数据前的所有数据
+        roomExcel: [],
         sortData: [] // 处理后的数据，每30个为一个数组
       }
     },
@@ -123,6 +128,7 @@
             console.log('出错了：：')
             return false
           }
+
         }
         fileReader.readAsBinaryString(file)
       },
@@ -153,64 +159,105 @@
           console.log('第' + i + '个考场')
           for (let j = 0; j < this.sortData[i].length; j++) {
             console.log(this.sortData[i][j].name, i + 1, j + 1)
-            this.$set(this.sortData[i][j], 'placeA', this.testPlaceName)
-            this.$set(this.sortData[i][j], 'placeB', this.testPlaceName)
             this.$set(this.sortData[i][j], 'subjectA', this.testSubjectA)
             this.$set(this.sortData[i][j], 'subjectB', this.testSubjectB)
             this.$set(this.sortData[i][j], 'dateA', this.testDate)
             this.$set(this.sortData[i][j], 'dateB', this.testDate)
             this.$set(this.sortData[i][j], 'timeA', this.testTimeA)
             this.$set(this.sortData[i][j], 'timeB', this.testTimeB)
-
-            if (this.roomNo < 10) {
-              this.$set(this.sortData[i][j], 'examinationRoomA', '0' + this.roomNo.toString())
-              this.$set(this.sortData[i][j], 'examinationRoomB', '0' + this.roomNo.toString())
+            let currentRoom = this.roomNo - 70 // 计算第二考场的序号，为currentRoom
+            if (this.roomNo <= 70) {
+              this.$set(this.sortData[i][j], 'examinationRoomName', '技师学院1号楼')
+              this.$set(this.sortData[i][j], 'placeA', this.testPlaceName + '1号楼')
+              if (this.roomNo < 10) {
+                this.$set(this.sortData[i][j], 'examinationRoomA', '0' + this.roomNo.toString())
+                this.$set(this.sortData[i][j], 'examinationRoomB', '0' + this.roomNo.toString())
+              } else {
+                this.$set(this.sortData[i][j], 'examinationRoomA', this.roomNo.toString())
+                this.$set(this.sortData[i][j], 'examinationRoomB', this.roomNo.toString())
+              }
             } else {
-              this.$set(this.sortData[i][j], 'examinationRoomA', this.roomNo.toString())
-              this.$set(this.sortData[i][j], 'examinationRoomB', this.roomNo.toString())
+              this.$set(this.sortData[i][j], 'examinationRoomName', '技师学院2号楼')
+              this.$set(this.sortData[i][j], 'placeA', this.testPlaceName + '2号楼')
+              if (currentRoom < 10) {
+                this.$set(this.sortData[i][j], 'examinationRoomA', '0' + currentRoom.toString())
+                this.$set(this.sortData[i][j], 'examinationRoomB', '0' + currentRoom.toString())
+              } else {
+                this.$set(this.sortData[i][j], 'examinationRoomA', currentRoom.toString())
+                this.$set(this.sortData[i][j], 'examinationRoomB', currentRoom.toString())
+              }
             }
 
             this.$set(this.sortData[i][j], 'examinationNumberA', j + 1 < 10 ? '0' + (j + 1) : (j + 1).toString())
             this.$set(this.sortData[i][j], 'examinationNumberB', j + 1 < 10 ? '0' + (j + 1) : (j + 1).toString())
-            this.$set(this.sortData[i][j], 'testNumber', this.createTestNumber(this.sortData[i][j].subject, this.sortData[i][j].examinationRoomA, this.sortData[i][j].examinationNumberA))
+            this.$set(this.sortData[i][j], 'testNumber', this.createTestNumber(this.sortData[i][j].subject, this.sortData[i][j].examinationRoomName, this.sortData[i][j].examinationRoomA, this.sortData[i][j].examinationNumberA))
           }
-        }
-      },
-      transformData() { // 将分配考场数据重新转化为可导出excel的全部数据
-        this.dataToExcel = []
-        for(let i = 0; i < this.sortData.length; i++) {
           this.dataToExcel = this.dataToExcel.concat(this.sortData[i])
         }
+        this.dataToRoomSetExcel = this.dataToRoomSetExcel.concat(this.sortData) // 重置前每次点击分配考场，都把数据塞到这个待导出全部的数组中
+      },
+      transformData() { // 将分配考场数据重新转化为可导出excel的全部数据
+        // this.dataToExcel = []
+        // for(let i = 0; i < this.sortData.length; i++) {
+        //   this.dataToExcel = this.dataToExcel.concat(this.sortData[i])
+        // }
         console.log(this.dataToExcel)
       },
       formatJson(filterVal, jsonData) {
         return jsonData.map(v => filterVal.map(j => v[j]))
       },
-      createTestNumber(subject, roomNo, examinationNumberA) {
+      createTestNumber(subject, examinationRoomName, roomNo, examinationNumberA) {
         console.log(roomNo, examinationNumberA)
-        switch (subject) {
-          case '综合管理类（A）':
-            return '202001' + roomNo + examinationNumberA
-          case '社会科学专技类（B）':
-            return '202002' + roomNo + examinationNumberA
-          case '自然科学专技类（C）':
-            return '202003' + roomNo + examinationNumberA
-          case '中小学教师类（D）小学教师':
-            return '202004' + roomNo + examinationNumberA
-          case '中小学教师类（D）中学教师':
-            return '202005' + roomNo + examinationNumberA
-          case '医疗卫生类（E）中医临床':
-            return '202006' + roomNo + examinationNumberA
-          case '医疗卫生类（E）西医临床':
-            return '202007' + roomNo + examinationNumberA
-          case '医疗卫生类（E）药剂':
-            return '202008' + roomNo + examinationNumberA
-          case '医疗卫生类（E）护理':
-            return '202009' + roomNo + examinationNumberA
-          case '医疗卫生类（E）医学技术':
-            return '202010' + roomNo + examinationNumberA
-          case '医疗卫生类（E）公共卫生管理':
-            return '202011' + roomNo + examinationNumberA
+        if (examinationRoomName === '技师学院2号楼') {
+          switch (subject) {
+            case '综合管理类（A）':
+              return '200102' + roomNo + examinationNumberA
+            case '社会科学专技类（B）':
+              return '200202' + roomNo + examinationNumberA
+            case '自然科学专技类（C）':
+              return '200302' + roomNo + examinationNumberA
+            case '中小学教师类（D）小学教师':
+              return '200402' + roomNo + examinationNumberA
+            case '中小学教师类（D）中学教师':
+              return '200502' + roomNo + examinationNumberA
+            case '医疗卫生类（E）中医临床':
+              return '200602' + roomNo + examinationNumberA
+            case '医疗卫生类（E）西医临床':
+              return '200702' + roomNo + examinationNumberA
+            case '医疗卫生类（E）药剂':
+              return '200802' + roomNo + examinationNumberA
+            case '医疗卫生类（E）护理':
+              return '200902' + roomNo + examinationNumberA
+            case '医疗卫生类（E）医学技术':
+              return '201002' + roomNo + examinationNumberA
+            case '医疗卫生类（E）公共卫生管理':
+              return '201102' + roomNo + examinationNumberA
+          }
+        } else {
+          switch (subject) {
+            case '综合管理类（A）':
+              return '200101' + roomNo + examinationNumberA
+            case '社会科学专技类（B）':
+              return '200201' + roomNo + examinationNumberA
+            case '自然科学专技类（C）':
+              return '200301' + roomNo + examinationNumberA
+            case '中小学教师类（D）小学教师':
+              return '200401' + roomNo + examinationNumberA
+            case '中小学教师类（D）中学教师':
+              return '200501' + roomNo + examinationNumberA
+            case '医疗卫生类（E）中医临床':
+              return '200601' + roomNo + examinationNumberA
+            case '医疗卫生类（E）西医临床':
+              return '200701' + roomNo + examinationNumberA
+            case '医疗卫生类（E）药剂':
+              return '200801' + roomNo + examinationNumberA
+            case '医疗卫生类（E）护理':
+              return '200901' + roomNo + examinationNumberA
+            case '医疗卫生类（E）医学技术':
+              return '201001' + roomNo + examinationNumberA
+            case '医疗卫生类（E）公共卫生管理':
+              return '201101' + roomNo + examinationNumberA
+          }
         }
       },
       exportExcel() {
@@ -225,11 +272,42 @@
         const filterVal = ['unit', 'subject', 'job', 'subject',
           'number', 'name',  'id',
           'subjectA', 'placeA', 'dateA', 'timeA',
-          'examinationRoomA', 'examinationNumberA','subjectB', 'placeB', 'dateB', 'timeB',
-          'examinationRoomB', 'examinationNumberB', 'testNumber', 'testNumber']
+          'examinationRoomA', 'examinationNumberA','subjectB', 'placeA', 'dateB', 'timeB',
+          'examinationRoomB', 'examinationNumberB', 'testNumber ', 'testNumber']
         const list = this.dataToExcel
         const data = this.formatJson(filterVal, list)
         export_json_to_excel(tHeader, data, this.dataToExcel[0].subject)
+      },
+      exportRoomExcel() {
+        this.roomExcel = []
+        for (let i = 0; i < this.dataToRoomSetExcel.length; i++) {
+          const str = this.dataToRoomSetExcel[i][0].testNumber + '~' + this.dataToRoomSetExcel[i][this.dataToRoomSetExcel[i].length - 1].testNumber
+          const obj = {
+            numberInterregional: str,
+            place: this.dataToRoomSetExcel[i][0].placeA,
+            subject: this.dataToRoomSetExcel[i][0].subject,
+            examinationRoom: this.dataToRoomSetExcel[i][0].examinationRoomA,
+            number: this.dataToRoomSetExcel[i].length
+
+          }
+          this.roomExcel.push(obj)
+        }
+        const { export_json_to_excel } = require('@/vendor/Export2Excel')
+        // 少一个准考证号，一个报考专业代码
+        const tHeader = ['准考证号区间', '考点', '考试类别', '考场号'
+          , '人数', '具体教室']
+        const filterVal = ['numberInterregional', 'place', 'subject', 'examinationRoom',
+          'number',  'id ']
+        const list = this.roomExcel
+        const data = this.formatJson(filterVal, list)
+        export_json_to_excel(tHeader, data, this.testDate + '考场安排表')
+      },
+      resetData() { // 重置所有上传的数据
+        this.roomNo = null
+        this.testNumber = null
+        this.dataToExcel = []
+        this.dataToRoomSetExcel = []
+        this.sortData = []
       }
     }
   }
@@ -243,6 +321,11 @@
     .header {
       margin-bottom: 30px;
       page-break-after: always;
+    }
+    .button-group {
+      margin-top: 10px;
+      display: flex;
+      justify-content: flex-start;
     }
     .container {
       margin-top: 20px;
